8ae59dee393de0a94793a9bd333419bd921f9dd8
Roman Akinfold <akinfold@gmail.com>
2013-11-22 00:15:17 +0400

    Add initial implementation of BaseTask and TaskQueue classes.

--- yadsimplemq/base.py
+++ yadsimplemq/base.py
@@ -2,10 +2,6 @@
 DEFAULT_ROUTING_KEY = 'yadsimplemq.tasks'
 
 
-class BaseTask(object):
-    pass
-
-
 class BaseMessageQueue(object):
     def __init__(self, broker_url, exchange, *args, **kwargs):
         """Create new message queue.
@@ -30,7 +26,7 @@ class BaseMessageQueue(object):
         """
         pass
 
-    def publish(self, message, routing_key=DEFAULT_ROUTING_KEY, *args, **kwargs):
+    def publish(self, message, routing_key=DEFAULT_ROUTING_KEY):
         """Publish new message to queue.
 
         :param message: Message to publish.
@@ -51,3 +47,103 @@ class BaseMessageQueue(object):
         :param routing_key: Routing key allowing to control which workers will execute task.
         """
         return (message for message in self.fetch(routing_key))
+
+
+class BaseTask(object):
+    def __init__(self, *arsg, **kwargs):
+        """Create new instance of task.
+
+        :param arsg: Sequential arguments for task. Will be passed to run() method.
+        :param kwargs: Keyword arguments for task. Will be passed to run() method.
+        """
+        self.args = arsg
+        self.kwargs = kwargs
+
+    @classmethod
+    def get_fqname(cls):
+        """Return fully qualified class name of task.
+        """
+        return '%s.%s' % (cls.__module__, cls.__name__)
+
+    def _run(self, *args, **kwargs):
+        """Execute exact work. Should be overridden in subclasses.
+        WARNING: Do not call this method directly, instead use apply() method.
+
+        :param arsg: Sequential arguments for task.
+        :param kwargs: Keyword arguments for task.
+        """
+        raise NotImplementedError('Must be implemented in subclasses.')
+
+    def apply(self, *args, **kwargs):
+        """Run task for execution and return its result.
+
+        :param arsg: Sequential arguments for task. Will override default arguments passed to __init__ if specified.
+        :param kwargs: Keyword arguments for task. Will override default arguments passed to __init__ if specified.
+        """
+        if args or kwargs:
+            return self._run(*args, **kwargs)
+        else:
+            return self._run(*self.args, **self.kwargs)
+
+
+class TaskQueue(object):
+    def __init__(self, backend):
+        """Create new instance of task queue.
+
+        :param backend: Instance of subclass of BaseMessageQueue.
+        """
+        self.backend = backend
+        self._task_cache = {}
+
+    def add_task_by_name(self, name, *args, **kwargs):
+        """Add new task to queue by its name and arguments.
+
+        :param name: Task name.
+        :param routing_key: Routing key allowing to control which workers will execute task.
+        :param args: Sequential arguments for task.
+        :param kwargs: Keyword arguments for task.
+        """
+        routing_key = kwargs.pop('routing_key', DEFAULT_ROUTING_KEY)
+        message = {
+            'name': name,
+            'args': args,
+            'kwargs': kwargs,
+            'in_progress': False,
+            'in_progress_since': None,
+            'done': False,
+            'done_since': None,
+        }
+        self.backend.publish(message, routing_key)
+
+    def add_task(self, task, routing_key=DEFAULT_ROUTING_KEY):
+        """Add new task to queue.
+
+        :param task: Fully initialized and ready to run instance of subclass of BaseTask.
+        :param routing_key: Routing key allowing to control which workers will execute task.
+        """
+        self.add_task_by_name(task.get_fqname(), routing_key, *task.args, **task.kwargs)
+
+    def raw_fetch(self, routing_key=DEFAULT_ROUTING_KEY):
+        """Fetch and return raw task from queue.
+
+        :param routing_key: Routing key allowing to control which workers will execute task.
+        """
+        return self.backend.fetch(routing_key)
+
+    @staticmethod
+    def _load_cls(name):
+        mod_name, cls_name = name.rsplit('.', 1)
+        mod = __import__(mod_name, globals(), locals(), fromlist=[cls_name])
+        return getattr(mod, cls_name)
+
+    def _load_task(self, name):
+        if name not in self._task_cache.keys():
+            self._task_cache[name] = self._load_cls(name)
+        return self._task_cache[name]
+
+    def fetch_task(self, routing_key=DEFAULT_ROUTING_KEY):
+        """Fetch task and return its initialized instance.
+        """
+        raw_task = self.raw_fetch(routing_key)
+        cls = self._load_task(raw_task['name'])
+        return cls(raw_task['args'], raw_task['kwargs'])
