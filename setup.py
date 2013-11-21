6505d5c7e2fce9070bcdb6180ddba2b3bd531636
Roman Akinfold <akinfold@gmail.com>
2013-11-22 02:29:00 +0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='yad-simple-mq',
+    version='0.0.1',
+    packages=['yadsimplemq'],
+    url='',
+    license='',
+    author='Roman Akinfold',
+    author_email='akinfold@gmail.com',
+    description='Simple implementation of task queue'
+)
